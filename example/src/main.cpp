8952574e1d2375ca6e3843200f65f905458f984a
Chen Lihui <lihui7115@gmail.com>
2020-05-19 16:09:48 +0800

    to learn from a bug report

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
 
+#include <filesystem>
+
 using namespace std;
 
 int main() {
-  string s = "hello";
+//  string s = "hello";
+//
+//  cout << s << endl;
+  
+  
+  // bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95048
+  std::filesystem::path p = L"测试";
 
-  cout << s << endl;
 
   return 0;
 }
