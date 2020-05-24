6f95c2ec9d5528f05d2a7a2ff74f8b3d10bb80a1
Chen Lihui <lihui7115@gmail.com>
2020-05-24 22:04:47 +0800

    c++ exception not only use gcc, but libunwind

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <exception>
+#include <cxxabi.h>
 
 using namespace std;
 
-void g() noexcept {
+void g() /* noexcept */ {
   throw 1;
 }
 
+void handle_eptr(std::exception_ptr eptr) // passing by value is ok
+{
+    try {
+        if (eptr) {
+            std::rethrow_exception(eptr);
+        }
+    } catch(const std::exception& e) {
+        std::cout << "Caught exception \"" << e.what() << "\"\n";
+    } catch (...) {
+      cout << "caught a exception by handle_eptr" << endl;
+    }
+}
+
+class A {
+public:
+  ~A() {
+    cout << "a dtor" << endl;
+  }
+};
+
 int main() {
-  class A;
+//   std::exception_ptr eptr;
+//  
+//  try {
+//    g();
+//  } catch (...) {
+//    cout << "caught a exception" << endl;
+//    
+//    eptr = std::current_exception();
+////    std::nested_exception();
+//    std::uncaught_exceptions();
+//  }
+//   
+//  handle_eptr(eptr);
   
   try {
-    g();
-  } catch (...) {
+    A a;
+    throw 1;
+  } catch (int) {
     cout << "caught a exception" << endl;
   }
 
